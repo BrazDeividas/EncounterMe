872bb596b3d4a939cf57341cbb1c6301e9f38673
Brazauskas Deividas <deividas.brazauskas@mif.stud.vu.lt>
2022-04-07 01:20:51 +0300

    temp

---
+++ MapApp/MapApp/MapApp/Pages/FriendsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using EncounterMe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace MapApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FriendsPage : Xamarin.Forms.TabbedPage
+    {
+        private IEnumerable<FriendDto> FriendList;
+        public FriendsPage()
+        {
+            InitializeComponent();
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+    }
+}
